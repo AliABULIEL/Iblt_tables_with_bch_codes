c43e94fd580a379840c04d8e117a36407206ec77
Ali Abu Liel <>
2023-12-30 18:07:15 +0200

    construction 2 is working

--- bch_iblt_first_construction_2.py
+++ bch_iblt_first_construction_2.py
@@ -75,61 +75,158 @@ class BchIbltConstruction2:
         data_bytes = str(data).encode()
         data_binary = [int(bit) for byte in data_bytes for bit in format(byte, '08b')]
 
-        # Debug: Print the binary data
-        print(f"Initial Binary Data: {data_binary}")
-
         k = self.bch.k
-        # Debug: Print the value of k
-        print(f"BCH message length k: {k}")
-
         # Adjust data length to match BCH code length k
         if len(data_binary) > k:
-            data_binary = data_binary[:k]
+            data_binary = data_binary[:k]  # Truncate if longer
         elif len(data_binary) < k:
-            data_binary += [0] * (k - len(data_binary))
-
-        # Debug: Print the adjusted binary data
-        print(f"Adjusted Binary Data: {data_binary}")
+            data_binary += [0] * (k - len(data_binary))  # Pad if shorter
 
         data_gf = self.bch.field(data_binary)
         encoded_data = self.bch.encode(data_gf)
-
-        # Debug: Print the encoded data
-        print(f"Encoded Data: {encoded_data}")
-
         return np.array(encoded_data, dtype=int)
+
+    # def encode_data(self, data):
+    #     # Convert data to binary format
+    #     data_bytes = str(data).encode()
+    #     data_binary = [int(bit) for byte in data_bytes for bit in format(byte, '08b')]
+    #     # print(f"Initial Binary Data: {data_binary} , length data {len(data_binary)}")  # Debug print
+    #
+    #     k = self.bch.k
+    #     # print(f"BCH message length k: {k}")  # Debug print
+    #
+    #     # Adjust data length to match BCH code length k
+    #     if len(data_binary) > k:
+    #         data_binary = data_binary[:k]
+    #     elif len(data_binary) < k:
+    #         data_binary += [0] * (k - len(data_binary))
+    #     # print(f"Adjusted Binary Data: {data_binary}")  # Debug print
+    #
+    #     data_gf = self.bch.field(data_binary)
+    #     encoded_data = self.bch.encode(data_gf)
+    #     # print(f"Encoded Data: {encoded_data}")  # Debug print
+    #
+    #     return np.array(encoded_data, dtype=int)
 
     def decode_data(self, encoded_data):
         try:
+            if len(encoded_data) > self.bch.n:
+                encoded_data = encoded_data[:self.bch.n]  # Trim to valid length
+
             decoded_data_gf = self.bch.decode(encoded_data)
             decoded_data_binary = decoded_data_gf.tolist()
             decoded_bytes = bytes(
-                int(''.join(map(str, decoded_data_binary[i:i + 8])), 2) for i in range(0, len(decoded_data_binary), 8))
-            return decoded_bytes.decode('utf-8', errors='ignore').rstrip('\x00')
-        except galois.GaloisException:
-            return None
+                int(''.join(map(str, decoded_data_binary[i:i + 8])), 2)
+                for i in range(0, len(decoded_data_binary), 8)
+            )
+            decoded_str = decoded_bytes.decode('utf-8', errors='ignore').rstrip('\x00')
+            return decoded_str if decoded_str else "Undecodable or no data"
+        except Exception as e:
+            print(f"Decoding error: {e}")
+            return "Undecodable or no data"
 
+    # def decode_data(self, encoded_data):
+    #     try:
+    #         # Ensure encoded_data is 1-D and the correct size
+    #         if len(encoded_data) > self.bch.n:
+    #             encoded_data = encoded_data[:self.bch.n]
+    #
+    #         decoded_data_gf = self.bch.decode(encoded_data)
+    #         decoded_data_binary = decoded_data_gf.tolist()
+    #         decoded_data_bytes = bytes(
+    #             int(''.join(map(str, decoded_data_binary[i:i + 8])), 2)
+    #             for i in range(0, len(decoded_data_binary), 8)
+    #         )
+    #         return decoded_data_bytes.decode('utf-8', errors='ignore').rstrip('\x00')
+    #     except Exception as e:
+    #         print(f"Decoding error: {e}")
+    #         return "Undecodable or no data"
+
+    # def insert(self, data):
+    #     encoded_data = self.encode_data(data)
+    #     data_hash = self.robust_hash_function(data)
+    #
+    #     # Ensure encoded_data is the same size as the table's row
+    #     if len(encoded_data) < self.table.shape[1]:
+    #         # Pad encoded_data to match the size
+    #         padding = np.zeros(self.table.shape[1] - len(encoded_data), dtype=int)
+    #         encoded_data = np.concatenate((encoded_data, padding))
+    #
+    #     for i in range(self.size):
+    #         if self.H2[i, data_hash % self.H2.shape[1]] != 0:
+    #             self.table[i] = (self.table[i] + encoded_data) % 2
     def insert(self, data):
         encoded_data = self.encode_data(data)
         data_hash = self.robust_hash_function(data)
 
+        # Resize encoded_data to match the size of table's row
+        encoded_data = np.resize(encoded_data, self.table.shape[1])
+
         for i in range(self.size):
             if self.H2[i, data_hash % self.H2.shape[1]] != 0:
                 self.table[i] = (self.table[i] + encoded_data) % 2
+        print(f"Data '{data}' inserted.")
+
+    # def insert(self, data):
+    #     encoded_data = self.encode_data(data)
+    #     data_hash = self.robust_hash_function(data)
+    #
+    #     if len(encoded_data) < self.table.shape[1]:
+    #         padding = np.zeros(self.table.shape[1] - len(encoded_data), dtype=int)
+    #         encoded_data = np.concatenate((encoded_data, padding))
+    #
+    #     for i in range(self.size):
+    #         if self.H2[i, data_hash % self.H2.shape[1]] != 0:
+    #             self.table[i] = (self.table[i] + encoded_data) % 2
+    #     print(f"Data inserted: {data}")
 
     def delete(self, data):
         encoded_data = self.encode_data(data)
         data_hash = self.robust_hash_function(data)
 
+        # Resize encoded_data to match the size of table's row
+        encoded_data = np.resize(encoded_data, self.table.shape[1])
+
         for i in range(self.size):
             if self.H2[i, data_hash % self.H2.shape[1]] != 0:
                 self.table[i] = (self.table[i] - encoded_data) % 2
+        print(f"Deleting data '{data}' from cell {i}")
+
+    # def delete(self, data):
+    #     encoded_data = self.encode_data(data)
+    #     data_hash = self.robust_hash_function(data)
+    #
+    #     if len(encoded_data) < self.table.shape[1]:
+    #         padding = np.zeros(self.table.shape[1] - len(encoded_data), dtype=int)
+    #         encoded_data = np.concatenate((encoded_data, padding))
+    #
+    #     for i in range(self.size):
+    #         if self.H2[i, data_hash % self.H2.shape[1]] != 0:
+    #             self.table[i] = (self.table[i] - encoded_data) % 2
+    #     print(f"Data deleted: {data}")
+
+    # def delete(self, data):
+    #     encoded_data = self.encode_data(data)
+    #     data_hash = self.robust_hash_function(data)
+    #
+    #     # Pad encoded_data to match the size of the table's row
+    #     if len(encoded_data) < self.table.shape[1]:
+    #         padding = np.zeros(self.table.shape[1] - len(encoded_data), dtype=int)
+    #         encoded_data = np.concatenate((encoded_data, padding))
+    #
+    #     for i in range(self.size):
+    #         if self.H2[i, data_hash % self.H2.shape[1]] != 0:
+    #             self.table[i] = (self.table[i] - encoded_data) % 2
+
+
 
     def list_entries(self):
         data_items = []
         for i, cell in enumerate(self.table):
-            if np.any(cell):
-                decoded_str = self.decode_data(cell)
-                if decoded_str:
-                    data_items.append(decoded_str)
+            print(f"Cell {i} contents: {cell}")
+            decoded_str = self.decode_data(cell)
+            print(f"Decoded Word at Cell {i}: '{decoded_str}'")
+            if decoded_str and decoded_str != "Undecodable or no data":
+                data_items.append(decoded_str)
+        return data_items
 
